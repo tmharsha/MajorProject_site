7525319632a5d5285ae20a80bb64a07f840e02b2
tmharsha <81552197+tmharsha@users.noreply.github.com>
2023-01-19 13:36:52 +0530

    completly changed js file

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-// Show the appropriate section when a button is clicked
-document.getElementById("home-button").addEventListener("click", function() {
-  document.getElementById("home").style.display = "block";
-  document.getElementById("about").style.display = "none";
-});
+window.onload = function() {
+  // Show the appropriate section when a button is clicked
+  document.getElementById("home-button").addEventListener("click", function() {
+    document.getElementById("home").style.display = "block";
+    document.getElementById("about").style.display = "none";
+  });
 
-document.getElementById("about-button").addEventListener("click", function() {
-  document.getElementById("home").style.display = "none";
-  document.getElementById("about").style.display = "block";
-});
+  document.getElementById("about-button").addEventListener("click", function() {
+    document.getElementById("home").style.display = "none";
+    document.getElementById("about").style.display = "block";
+  });
+};
